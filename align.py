6d883acaf04639c15954bf5bd523180cdc7376bc
Marshall Thompson <mdt_@gwu.edu>
2022-02-02 16:23:52 -0500

    auto align!

---
+++ align.py
@@ -0,0 +1,84 @@
+import cv2
+import numpy as np
+
+
+from filter import gaussian_filter
+from sklearn.preprocessing import normalize
+import skimage as sk
+
+def sobel_edge_detection(img_blur):
+    sobelxy = cv2.Sobel(src=img_blur, ddepth=cv2.CV_64F, dx=1, dy=1, ksize=5) # Combined X and Y Sobel Edge Detection
+    return sobelxy
+
+def preprocess(img):
+    # grayscale
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # blur
+    blurred = gaussian_filter(gray, (3,3), 0, 0)
+
+    edges = sobel_edge_detection(blurred)
+    return edges
+
+def pyramids(img, height=4):
+    """
+    Takes in the three color channels and downsamples by half height times.
+    Returns dictionary of the pyramid downsampling
+    """
+    pyramids = []
+    for i in range(height-1,-1, -1):
+        factor = 0.5**i
+        pyramids.append(cv2.resize(img, (0,0), fx=factor, fy=factor))
+
+    return pyramids
+
+def sse(i1, i2):
+    return np.sum( np.square(i2 - i1) )
+
+def ncc(i1, i2):
+    ni1 = sk.img_as_uint(normalize(i1))
+    ni2 = sk.img_as_uint(normalize(i2))
+
+    return np.sum( np.dot( ni1, ni2 ) )
+
+def align(img1, img2, displacement_size, freeze_x=False,freeze_y=False):
+    sses = []
+    displacements = []
+    for x in range(-displacement_size, displacement_size):
+        if  freeze_x and x != 0:
+            continue
+        for y in range(-displacement_size,displacement_size):
+            if freeze_y and y != 0:
+                continue
+            displaced_i2 = np.roll(img2, (x, y), axis=(1,0))
+
+            sses.append(sse(img1, displaced_i2))
+            # sses.append(ncc(img1, displaced_i2))
+            displacements.append((x,y))
+
+    best_idx = np.argmin(sses)
+
+    return displacements[best_idx]
+
+def align_handler(img1, img2, pyramid_height, factor, freeze_x=False,freeze_y=False):
+    edges1 = preprocess(img1)
+    edges2 = preprocess(img2)
+
+    pyramid1 = pyramids(edges1, height=pyramid_height)
+    pyramid2 = pyramids(edges2, height=pyramid_height)
+
+    dy, dx = 0, 0
+    for i in range(pyramid_height):
+        displacement_size = int(factor*pyramid1[pyramid_height-1].shape[0]) if i == 0 else 2
+
+        disp = align(pyramid1[i], pyramid2[i], displacement_size, freeze_y=freeze_y, freeze_x=freeze_x)
+        dx += disp[0]*2
+        dy += disp[1]*2
+    print(dx,dy)
+    aligned_i2 = np.roll(img2, (dx, dy), axis=(1,0))
+    return aligned_i2
+
+
+
+
+
+
--- driver.py
+++ driver.py
@@ -1,16 +1,20 @@
-import imp
+from tkinter.tix import Tree
 from image import Image
 import skimage.io as skio
 import numpy as np
+from align import align_handler
+
+
 def main():
     p1, p2 = '/Users/mdt/projects/computational-photography-p2/images/cat_dog/cat_bigger.jpeg','/Users/mdt/projects/computational-photography-p2/images/cat_dog/dog_bigger.jpeg'
 
     i1, i2 = Image(p1), Image(p2)
-    i1.image = np.roll(i1.image, (-100, 0), axis=(0,1))
+    # i1.image = np.roll(i1.image, (-100, 0), axis=(0,1))
+    i1.image = align_handler(i2.image, i1.image, 3, factor=0.12, freeze_y=False, freeze_x=True)
     f1 = i1.apply_filter('gaussian', (101,101), 10, 10, frequency='low')
     f2 = i2.apply_filter('gaussian', (101,101), 9,9, frequency='high')
     # skio.imshow(np.concatenate((f1, f2), axis=1))
-    skio.imsave('./images/results/cat_dog/cat_dog.png',f1 + f2)
+    skio.imsave('./images/results/cat_dog/cat_dog_auto_align.png',f1 + f2)
     # print()
 
 if __name__ == '__main__':
